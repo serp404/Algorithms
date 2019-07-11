b66c5709560390109aa413beaf41c50b7ffdf7f6
Sergey Petrovich <52720435+serp404@users.noreply.github.com>
2019-07-11 13:50:56 +0300

    Create selection_sort.cpp

---
+++ sorting_algorithms/selection_sort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template<typename T>
+void selection_sort(std::vector<T>& array, bool ascending=true) {
+    for (size_t i = 0; i != array.size(); ++i) {
+        size_t pos = i;
+        for (size_t j = i; j != array.size(); ++j) {
+            if (array[j] < array[pos])
+                pos = j;
+        }
+        std::swap(array[i], array[pos]);
+    }
+
+    if (!ascending)
+        std::reverse(array.begin(), array.end());
+}
+
+
+
+template<typename T>
+std::ostream& operator << (std::ostream& out, const std::vector<T>& array) {
+    out << "{";
+    for (size_t i = 0; i != array.size(); ++i) {
+        if (i != array.size() - 1)
+            out << " " << array[i] << ",";
+        else
+            out << " " << array[i] << " ";
+    }
+    out << "}";
+    return out;
+}
+
+int main() {
+    std::vector<size_t> a1 = {5, 1, 7, 8, 2, 4};
+    selection_sort(a1);
+    std::cout << a1 << "\n";
+
+    std::vector<double> a2 = {6.5, 4.1, 2.71, 3.14, 10};
+    selection_sort(a2, false);
+    std::cout << a2 << "\n";
+}
+
+// Output:
+//{ 1, 2, 4, 5, 7, 8 }
+//{ 10, 6.5, 4.1, 3.14, 2.71 }
